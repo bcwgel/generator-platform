41444713e238f7a20f719262f2cdf18a7641b7a1
wbcshy <wbc0128@163.com>
2019-09-13 04:21:19 +0800

    初次上传

---
+++ gen-admin/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO "DATABASE_INFO"("DB_ID","DB_NAME","JDBC_DRIVER","JDBC_URL","PASSWORD","USER_NAME","CREATE_TIME")VALUES(1,'daikin','com.mysql.jdbc.Driver','jdbc:mysql://47.96.16.46:3306/daikin?autoReconnect=true&useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=CONVERT_TO_NULL&useSSL=false&serverTimezone=CTT','Yu_8168907','root',PARSEDATETIME('01-30-2019','dd-MM-yyyy'));
